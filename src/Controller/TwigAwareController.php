ea60aa764ebee88a67123b50ba05910dccdde138
Sylwia Szewczyk <josephine.rybnik@gmail.com>
2024-11-20 11:30:22 +0100

    microservices csp fix

--- src/Controller/TwigAwareController.php
+++ src/Controller/TwigAwareController.php
@@ -101,7 +101,8 @@ class TwigAwareController extends AbstractController
             $recordCsp = $this->forward('Bolt\Controller\Frontend\DetailController::getCspHeader');
             if($recordCsp){
                 $csp = json_decode($recordCsp->getContent(), true);
-                $response->headers->set('Content-Security-Policy', trim($csp['csp']));
+                if( $csp['csp'] )
+                    $response->headers->set('Content-Security-Policy', trim($csp['csp']));
             }
         }
 
